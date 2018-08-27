94ab56f8c669d0686529f85baf43a0aa188f37f7
Deokseong Seo <heyhi16@gmail.com>
2018-08-27 17:57:31 +0900

    Update Text_CNN_train.py

--- Ch02_TextCNN/Text_CNN_train.py
+++ Ch02_TextCNN/Text_CNN_train.py
@@ -24,11 +24,12 @@ information = ''
 FLAGS.WRITER += information
 
 
+
 ################################################################################
 # DATA LOAD
 ################################################################################
 TRAIN_DOC, TRAIN_LABEL, TRAIN_LABEL_POS, TRAIN_LABEL_NEG, TEST_DOC, TEST_LABEL, TEST_LABEL_POS, TEST_LABEL_NEG = data_load.data_load()
-JM = utils.lookup_JM(FLAGS.MAXLEN, FLAGS.INPUT_WIDTH, FLAGS.INPUT_DEPTH)
+JM = utils.lookup_JM(FLAGS.INPUT_WIDTH, FLAGS.INPUT_DEPTH)
 
 
 
@@ -63,7 +64,7 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
     ################################################################
     index = utils.sampler(LABEL_POS=TRAIN_LABEL_POS, LABEL_NEG=TRAIN_LABEL_NEG, BATCH_SIZE=FLAGS.BATCH_SIZE)
     batch_input, batch_label = utils.generate_batch_jaso(INDEX=index, MODEL=model, DOC=TRAIN_DOC,
-                                                         LABEL=TRAIN_LABEL, MAXLEN=FLAGS.MAXLEN, SESS=sess, ATTENTION=False)
+                                                         LABEL=TRAIN_LABEL, MAXLEN=FLAGS.INPUT_WIDTH, SESS=sess)
     _ = sess.run([model.optm],
                  feed_dict={model.X: batch_input,
                             model.Y: batch_label,
@@ -76,7 +77,7 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
     ################################################################
     if i % 10 == 0:
         ################################################################
-        # Test batch LOSS CHECK
+        # Train batch LOSS CHECK
         ################################################################
         tr_loss, tr_acc, tr_merged = sess.run([model.cross_entropy, model.accuracy, model.merge],
                                              feed_dict={model.X: batch_input,
@@ -90,7 +91,7 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
         ################################################################
         index = utils.sampler(LABEL_POS=TEST_LABEL_POS, LABEL_NEG=TEST_LABEL_NEG, BATCH_SIZE=FLAGS.BATCH_SIZE)
         batch_input, batch_label = utils.generate_batch_jaso(INDEX=index, MODEL=model, DOC=TEST_DOC,
-                                                             LABEL=TEST_LABEL, MAXLEN=FLAGS.MAXLEN, SESS=sess, ATTENTION=False)
+                                                             LABEL=TEST_LABEL, MAXLEN=FLAGS.INPUT_WIDTH, SESS=sess)
         ts_loss, ts_acc, ts_merged = sess.run([model.cross_entropy, model.accuracy, model.merge],
                                               feed_dict={model.X: batch_input,
                                                          model.Y: batch_label,
@@ -103,9 +104,10 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
         ################################################################
         print("Iter: {iter:08} / Epoch: {EP} |##| LR: {LR:0.15f} |##|  tr_LOSS: {tr_LOSS:0.8f} |##|  tr_acc: {tr_ACC:0.8f} |##|  ts_LOSS: {ts_loss:0.8f} |##|  ts_acc: {ts_acc:0.8f}".format(
                 iter=i, EP=epoch, LR=FLAGS.lr_value, tr_LOSS=tr_loss, tr_ACC=tr_acc, ts_loss=ts_loss, ts_acc=ts_acc))
-        FLAGS.Check_Loss = np.delete(FLAGS.Check_Loss, [0])
-        FLAGS.Check_Loss = np.append(FLAGS.Check_Loss, tr_loss)
+        FLAGS.Check_Loss = FLAGS.Check_Loss[1:]
+        FLAGS.Check_Loss.append(round(tr_loss, 4))
         print(FLAGS.Check_Loss)
+
 
 
     ##############################################################################################################
@@ -118,7 +120,14 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
 # Save parameters
 ################################################################################
 # Save Weights
-if "./Saver/{}".format(FLAGS.WRITER) not in os.listdir("./Saver"):
+if "Saver" not in os.listdir("./"):
+    os.makedirs("./Saver")
+if FLAGS.WRITER not in os.listdir("./Saver"):
     os.makedirs("./Saver/{}".format(FLAGS.WRITER))
 saver = tf.train.Saver()
 saver.save(sess, "./Saver/{}/{}.ckpt".format(FLAGS.WRITER, FLAGS.WRITER))
+
+# tensorboard
+# cmd창에서 summary 있는 폴더로 가서
+# tensorboard --logdir=./
+# tensorboard --logdir=./ --host=localhost --port=8088
