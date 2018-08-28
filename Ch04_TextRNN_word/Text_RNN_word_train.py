4b6acac8052d2f8e32892625c9e432a321b0e23c
Deokseong Seo <heyhi16@gmail.com>
2018-08-28 09:21:36 +0900

    Update Text_RNN_word_train.py

--- Ch04_TextRNN_word/Text_RNN_word_train.py
+++ Ch04_TextRNN_word/Text_RNN_word_train.py
@@ -65,7 +65,7 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
     ################################################################
     index = utils.sampler(LABEL_POS=TRAIN_LABEL_POS, LABEL_NEG=TRAIN_LABEL_NEG, BATCH_SIZE=FLAGS.BATCH_SIZE)
     batch_input, batch_label = utils.generate_batch_word(INDEX=index, VOCAB_PROCESSOR=vocab_processor,
-                                                         DOC=TRAIN_DOC, LABEL=TRAIN_LABEL, ATTENTION=False)
+                                                         DOC=TRAIN_DOC, LABEL=TRAIN_LABEL)
     seq_length, _ = utils.length(batch_input)
 
     _ = sess.run([model.optm],
@@ -83,7 +83,7 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
     ################################################################
     if i % 10 == 0:
         ################################################################
-        # Test batch LOSS CHECK
+        # Train batch LOSS CHECK
         ################################################################
         tr_loss, tr_acc, tr_merged = sess.run([model.cross_entropy, model.accuracy, model.merge],
                                               feed_dict={model.X_idx: batch_input,
@@ -100,7 +100,7 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
         ################################################################
         index = utils.sampler(LABEL_POS=TEST_LABEL_POS, LABEL_NEG=TEST_LABEL_NEG, BATCH_SIZE=FLAGS.TEST_BATCH)
         batch_input, batch_label = utils.generate_batch_word(INDEX=index, VOCAB_PROCESSOR=vocab_processor,
-                                                             DOC=TEST_DOC, LABEL=TEST_LABEL, ATTENTION=False)
+                                                             DOC=TEST_DOC, LABEL=TEST_LABEL)
         seq_length, _ = utils.length(batch_input)
 
         ts_loss, ts_acc, ts_merged = sess.run([model.cross_entropy, model.accuracy, model.merge],
@@ -118,8 +118,8 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
         ################################################################
         print("Iter: {iter:08} / Epoch: {EP} |##| LR: {LR:0.15f} |##|  tr_LOSS: {tr_LOSS:0.8f} |##|  tr_acc: {tr_ACC:0.8f} |##|  ts_LOSS: {ts_loss:0.8f} |##|  ts_acc: {ts_acc:0.8f}".format(
                 iter=i, EP=epoch, LR=FLAGS.lr_value, tr_LOSS=tr_loss, tr_ACC=tr_acc, ts_loss=ts_loss, ts_acc=ts_acc))
-        FLAGS.Check_Loss = np.delete(FLAGS.Check_Loss, [0])
-        FLAGS.Check_Loss = np.append(FLAGS.Check_Loss, tr_loss)
+        FLAGS.Check_Loss = FLAGS.Check_Loss[1:]
+        FLAGS.Check_Loss.append(round(tr_loss, 4))
         print(FLAGS.Check_Loss)
 
 
@@ -132,8 +132,8 @@ for i in range(Num_of_Iterlation * FLAGS.NUM_OF_EPOCH):
 # Save parameters
 ################################################################################
 # Save Weights
-if "./Saver/{}".format(FLAGS.WRITER) not in os.listdir("./Saver"):
+if FLAGS.WRITER not in os.listdir("./Saver"):
     os.makedirs("./Saver/{}".format(FLAGS.WRITER))
 saver = tf.train.Saver()
 saver.save(sess, "./Saver/{}/{}.ckpt".format(FLAGS.WRITER, FLAGS.WRITER))
-# C:/Users/DeokseongSeo/Desktop/{}.ckpt
+
