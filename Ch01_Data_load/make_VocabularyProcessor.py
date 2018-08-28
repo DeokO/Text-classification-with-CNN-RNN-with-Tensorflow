c188f87ca74c6fd9ac7fde1b9d634562ada9bea9
Deokseong Seo <heyhi16@gmail.com>
2018-08-28 09:29:38 +0900

    Update make_VocabularyProcessor.py

--- Ch01_Data_load/make_VocabularyProcessor.py
+++ Ch01_Data_load/make_VocabularyProcessor.py
@@ -31,6 +31,9 @@ max_doc_length = max([len(x.split(" ")) for x in TRAIN_DOC])
 # Create the vocabularyprocessor object, setting the max lengh of the documents.
 vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(max_doc_length, min_frequency=5)
 
+# save vocabularyprocessor object
+vocab_processor.save('./Ch01_Data_load/data/VocabularyProcessor')
+
 # Transform the documents using the vocabulary.
 x = np.array(list(vocab_processor.fit_transform(TRAIN_DOC)))
 
@@ -38,5 +41,38 @@ x = np.array(list(vocab_processor.fit_transform(TRAIN_DOC)))
 vocab_dict = vocab_processor.vocabulary_._mapping
 # len(vocab_dict) #72844
 
-# save vocabularyprocessor object
-vocab_processor.save('./Ch01_Data_load/data/VocabularyProcessor')
+
+
+
+
+
+
+
+
+# # 예시용 코드 (출처: https://stackoverflow.com/questions/40661684/tensorflow-vocabularyprocessor)
+# import numpy as np
+# from tensorflow.contrib import learn
+#
+# x_text = ['This is a cat','This must be boy', 'This is a a dog']
+# max_document_length = max([len(x.split(" ")) for x in x_text])
+#
+# ## Create the vocabularyprocessor object, setting the max lengh of the documents.
+# vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+#
+# ## Transform the documents using the vocabulary.
+# x = np.array(list(vocab_processor.fit_transform(x_text)))
+#
+# ## Extract word:id mapping from the object.
+# vocab_dict = vocab_processor.vocabulary_._mapping
+#
+# ## Sort the vocabulary dictionary on the basis of values(id).
+# ## Both statements perform same task.
+# #sorted_vocab = sorted(vocab_dict.items(), key=operator.itemgetter(1))
+# sorted_vocab = sorted(vocab_dict.items(), key=lambda x: x[1])
+#
+# ## Treat the id's as index into list and create a list of words in the ascending order of id's
+# ## word with id i goes at index i of the list.
+# vocabulary = list(list(zip(*sorted_vocab))[0])
+#
+# print(vocabulary)
+# print(x)
